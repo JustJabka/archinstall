09d50fcfea61ae2f474c1319906700cefa1fc515
rtxsqd <91340996+rtxsquad@users.noreply.github.com>
2025-01-24 23:50:48 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -9,6 +9,19 @@ echo "--- Thank you for using our scripts ---"
 echo "--- This script will help you complete the system setup ---"
 echo "Which browser will be installed? (select a number)"
 echo "1. firefox 2. chromium"
+while true; do
+read -p "Введите номер: " numbrow
+
+if [ $numbrow -eq 1 ]; then
+    sudo pacman -S firefox
+    break
+  elif [ $numbrow -eq 2 ]; then
+    sudo pacman -S chromium
+    break
+  else
+    echo "Uncorrected input."
+  fi
+done
 
 sudo pacman -S --noconfirm hyprland ly kitty git
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-firacode-nerd
