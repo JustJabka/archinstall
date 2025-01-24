63d16509c9b53bf3f30f2c18184445aebdcc2bb2
rtxsqd <91340996+rtxsquad@users.noreply.github.com>
2025-01-25 00:19:31 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -8,9 +8,9 @@ fi
 echo "--- Thank you for using our scripts ---"
 echo "--- This script will help you complete the system setup ---"
 echo "Which browser will be installed? (select a number)"
-echo "1. firefox 2. chromium"
+echo "1. firefox 2. chromium 3. skip"
 while true; do
-read -p "Введите номер: " numbrow
+read -p "enter the selection: " numbrow
 
 if [ $numbrow -eq 1 ]; then
     sudo pacman -S firefox
@@ -18,13 +18,17 @@ if [ $numbrow -eq 1 ]; then
   elif [ $numbrow -eq 2 ]; then
     sudo pacman -S chromium
     break
+  elif [ $numbrow -eq 3 ]; then
+    break
   else
     echo "Uncorrected input."
   fi
 done
 
-sudo pacman -S --noconfirm hyprland ly kitty git
+sudo pacman -Syu
+sudo pacman -S --noconfirm hyprland ly kitty git yay flatpak
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-firacode-nerd
 sudo fc-cache -fv
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 sudo systemctl --now enable ly.service
