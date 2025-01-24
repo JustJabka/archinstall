4e36ae09dbf21068f2af006bba680dda5482886e
rtxsqd <91340996+rtxsquad@users.noreply.github.com>
2025-01-24 23:33:55 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,12 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-sudo pacman -S --noconfirm hyprland ly kitty git firefox
+echo "--- Thank you for using our scripts ---"
+echo "--- This script will help you complete the system setup ---"
+echo "Which browser will be installed? (select a number)"
+echo "1. firefox 2. chromium"
+
+sudo pacman -S --noconfirm hyprland ly kitty git
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-firacode-nerd
 sudo fc-cache -fv
 
