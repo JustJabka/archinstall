9f53acab51bdef11eecdec13070b85a79d776f6a
rtxsqd <91340996+rtxsquad@users.noreply.github.com>
2025-01-25 00:38:05 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,6 +5,8 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
+sudo pacman -S xorg xorg-server xorg-xinit xorg-apps mesa-libgl
+
 echo "--- Thank you for using our scripts ---"
 echo "--- This script will help you complete the system setup ---"
 echo "Which browser will be installed? (select a number)"
@@ -25,8 +27,48 @@ if [ $numbrow -eq 1 ]; then
   fi
 done
 
-sudo pacman -Syu
-sudo pacman -S --noconfirm hyprland ly kitty git yay flatpak
+echo "Which video drivers will be installed? (select a number)"
+echo "1. Nvidia 2. AMD 3. Intel 4. skip"
+while true; do
+read -p "enter the selection: " numdrivers
+
+if [ $numdrivers -eq 1 ]; then
+    sudo pacman -S nvidia nvidia-settings
+    break
+  elif [ $numdrivers -eq 2 ]; then
+    sudo pacman -S xf86-video-ati
+    break
+  elif [ $numdrivers -eq 3 ]; then
+    sudo pacman -S xf86-video-intel
+    break
+  elif [ $numdrivers -eq 4 ]; then
+    break
+  else
+    echo "Uncorrected input."
+  fi
+done
+
+echo "Which DE will be installed? (select a number)"
+echo "1. Gnome 2. KDE 3. skip"
+while true; do
+read -p "enter the selection: " numde
+
+if [ $numde -eq 1 ]; then
+    sudo pacman -S gnome gdm ttf-dejavu gnome-extra
+    sudo systemctl enable gdm
+    break
+  elif [ $numde -eq 2 ]; then
+    sudo pacman -S plasma sddm sddm-kcm kde-applications
+    sudo systemctl enable sddm
+    break
+  elif [ $numde -eq 3 ]; then
+    break
+  else
+    echo "Uncorrected input."
+  fi
+done
+
+sudo pacman -S --noconfirm hyprland ly kitty git yay flatpak vlc pulseaudio 
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-firacode-nerd
 sudo fc-cache -fv
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
